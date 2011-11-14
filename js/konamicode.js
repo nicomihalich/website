5502f369890f87c8b487c06f1245141bfee063fd
Nico Mihalich <nmihalich@gmail.com>
2011-11-14 13:39:27 -0500

    deleted small script

--- js/konamicode.js
+++
@@ -1,7 +0,0 @@
-      konami = new Konami()
-          konami.code = function(){
-                        $("#main").fadeOut(function(){
-                                        $("#easteregg").fadeIn()  
-                                                })
-                                }
-    konami.load()
